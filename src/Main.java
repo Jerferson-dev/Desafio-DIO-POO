630332c74d873d7af3d78d6cd59e638a5cb424a2
dev-jerferson <jerfersonsantos30@gmail.com>
2026-01-13 13:25:29 -0300

    New class: Mentoria, Curso.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Curso curso1 = new Curso("curso java", "descrição curso java", 8 );
+
+
+        Curso curso2 = new Curso("curso js","decrição curso js", 4);
+
+
+
+        System.out.println("---------------------");
+        Mentoria mentoria = new Mentoria( "mentoria de java", "descrição de java", LocalDate.now());
+       
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+
 
 
 
